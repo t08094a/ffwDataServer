edf52e16d768300d072a2dcbfcbd752294296e89
t08094a <jochen.faehnlein@ff-ipsheim.de>
2016-09-19 17:36:28 +0200

    login / logout

--- src/main/java/com/leif/ffDataServer/config/ResourceSecurityConfiguration.java
+++ src/main/java/com/leif/ffDataServer/config/ResourceSecurityConfiguration.java
@@ -3,10 +3,13 @@ package com.leif.ffDataServer.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableGlobalAuthentication
+@EnableWebSecurity
 public class ResourceSecurityConfiguration extends WebSecurityConfigurerAdapter
 {
 	@Override
@@ -16,6 +19,17 @@ public class ResourceSecurityConfiguration extends WebSecurityConfigurerAdapter
 			.antMatchers("/").permitAll()
 			.antMatchers("/api/**").authenticated()
 			.and()
-			.formLogin();
+			.formLogin()
+				.defaultSuccessUrl("/api", true)
+				.permitAll()
+			.and()
+				.logout()
+				.logoutUrl("/logout/")
+				.invalidateHttpSession(true)
+				.logoutUrl("/")
+				.deleteCookies("JSESSIONID")
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.and()
+				.csrf();
 	}
 }

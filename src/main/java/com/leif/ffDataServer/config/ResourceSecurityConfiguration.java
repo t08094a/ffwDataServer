4e10885e88dbc2599bf49cc92440be3e8cc5a5dc
t08094a <jochen.faehnlein@ff-ipsheim.de>
2016-09-19 15:55:37 +0200

    erweitert: security (primitiv)

---
+++ src/main/java/com/leif/ffDataServer/config/ResourceSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.leif.ffDataServer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalAuthentication
+public class ResourceSecurityConfiguration extends WebSecurityConfigurerAdapter
+{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		http.authorizeRequests()
+			.antMatchers("/").permitAll()
+			.antMatchers("/api/**").authenticated()
+			.and()
+			.formLogin();
+	}
+}
